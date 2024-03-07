cf4e20a9596c426b511048415210da63c0dac34b
游艺(youyi) <youyichannel@163.com>
2024-03-07 13:55:12 +0800

    feat(rpc): add simple local registry

--- example/example-provider/src/main/java/com/youyi/example/provider/EasyProviderExample.java
+++ example/example-provider/src/main/java/com/youyi/example/provider/EasyProviderExample.java
@@ -1,12 +1,25 @@
 package com.youyi.example.provider;
 
+import com.youyi.example.common.service.UserService;
+import com.youyi.rpc.registry.LocalRegistry;
+import com.youyi.rpc.server.HttpServer;
+import com.youyi.rpc.server.VertxHttpServer;
+
 /**
  * 简易服务提供者示例
  *
  * @author <a href="https://github.com/dingxinliang88">youyi</a>
  */
 public class EasyProviderExample {
+
     public static void main(String[] args) {
-        // 提供服务
+
+        // 注册服务
+        LocalRegistry.registry(UserService.class.getName(), UserServiceImpl.class);
+
+        // 启动 Web 服务
+        HttpServer httpServer = new VertxHttpServer();
+        // for test: curl -X GET http://localhost:8080
+        httpServer.doStart(8080);
     }
 }
