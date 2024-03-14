b698237684fd290e62d647e8e655a91e66bed964
游艺(youyi) <youyichannel@163.com>
2024-03-14 10:20:27 +0800

    feat(tolerant): load tolerant strategy using spi and using retry && tolerant

--- easy-rpc-core/src/main/java/com/youyi/rpc/config/Config.java
+++ easy-rpc-core/src/main/java/com/youyi/rpc/config/Config.java
@@ -1,6 +1,7 @@
 package com.youyi.rpc.config;
 
 import com.youyi.rpc.fault.retry.RetryStrategyKeys;
+import com.youyi.rpc.fault.tolerant.TolerantStrategyKeys;
 import com.youyi.rpc.lb.LoadBalancerKeys;
 import com.youyi.rpc.serializer.SerializerKeys;
 import lombok.Data;
@@ -22,4 +23,5 @@ public class Config {
     private RegistryConfig registry = new RegistryConfig();
     private String loadBalancer = LoadBalancerKeys.ROUND_ROBIN;
     private String retry = RetryStrategyKeys.NO;
+    private String tolerant = TolerantStrategyKeys.FAIL_FAST;
 }
---
+++ easy-rpc-core/src/main/java/com/youyi/rpc/fault/tolerant/TolerantStrategyFactory.java
@@ -0,0 +1,34 @@
+package com.youyi.rpc.fault.tolerant;
+
+import com.youyi.rpc.exception.NoSuchLoadClassException;
+import com.youyi.rpc.spi.SpiLoader;
+
+/**
+ * 容错策略工厂
+ *
+ * @author <a href="https://github.com/dingxinliang88">youyi</a>
+ */
+public class TolerantStrategyFactory {
+
+    /**
+     * 获取容错策略
+     *
+     * @param key key
+     * @return tolerant strategy
+     */
+    public static TolerantStrategy getTolerantStrategy(String key) {
+        TolerantStrategy tolerantStrategy;
+        try {
+            tolerantStrategy = SpiLoader.getInstance(TolerantStrategy.class, key);
+        } catch (NoSuchLoadClassException e) {
+            init();
+            tolerantStrategy = SpiLoader.getInstance(TolerantStrategy.class, key);
+        }
+        return tolerantStrategy;
+    }
+
+    public synchronized static void init() {
+        SpiLoader.load(TolerantStrategy.class);
+    }
+
+}
