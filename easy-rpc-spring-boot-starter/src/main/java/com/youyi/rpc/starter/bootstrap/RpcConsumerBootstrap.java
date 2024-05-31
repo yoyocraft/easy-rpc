4e280e303b3cf6a6029b9865eaff449c9693188d
youyi <youyichannel@163.com>
2024-05-31 18:55:42 +0800

    chore(rpc): chore dependency versions and bootstrap

---
+++ easy-rpc-spring-boot-starter/src/main/java/com/youyi/rpc/starter/bootstrap/RpcConsumerBootstrap.java
@@ -0,0 +1,59 @@
+package com.youyi.rpc.starter.bootstrap;
+
+import com.youyi.rpc.exception.RpcException;
+import com.youyi.rpc.proxy.ServiceProxyFactory;
+import com.youyi.rpc.starter.annotation.RpcReference;
+import java.lang.reflect.Field;
+import lombok.NonNull;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * RPC 服务消费者
+ * <p>
+ * 获取到包含 @RpcReference 注解的类，为其注入属性
+ *
+ * @author <a href="https://github.com/dingxinliang88">youyi</a>
+ */
+public class RpcConsumerBootstrap implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName)
+            throws BeansException {
+        Class<?> beanClazz = bean.getClass();
+        // 遍历对象的所有属性
+        Field[] declaredFields = beanClazz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            RpcReference rpcReference = field.getAnnotation(RpcReference.class);
+            if (rpcReference == null) {
+                continue;
+            }
+            // 为属性生成代理对象
+            Class<?> interfaceClass = rpcReference.interfaceClass();
+            if (interfaceClass == void.class) {
+                interfaceClass = field.getType();
+            }
+            field.setAccessible(true);
+            try {
+                boolean mock = rpcReference.mock();
+                if (mock) {
+                    field.set(bean, ServiceProxyFactory.getMockProxy(interfaceClass));
+                } else {
+                    String serviceVersion = rpcReference.version();
+                    String serviceGroup = rpcReference.group();
+                    String loadBalancer = rpcReference.loadBalancer();
+                    long timeout = rpcReference.timeout();
+                    Object proxy = ServiceProxyFactory.getProxy(interfaceClass, serviceVersion,
+                            serviceGroup, loadBalancer, timeout);
+                    field.set(bean, proxy);
+                }
+            } catch (IllegalAccessException e) {
+                throw new RpcException(
+                        "failed to set rpc proxy in field: " + field.getName(), e);
+            } finally {
+                field.setAccessible(false);
+            }
+        }
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
---
+++ easy-rpc-spring-boot-starter/src/main/java/com/youyi/rpc/starter/bootstrap/RpcProviderBootstrap.java
@@ -0,0 +1,68 @@
+package com.youyi.rpc.starter.bootstrap;
+
+import com.youyi.rpc.RpcApplication;
+import com.youyi.rpc.config.ApplicationConfig;
+import com.youyi.rpc.config.RegistryConfig;
+import com.youyi.rpc.exception.RpcException;
+import com.youyi.rpc.model.ServiceMetadata;
+import com.youyi.rpc.registry.LocalRegistry;
+import com.youyi.rpc.registry.Registry;
+import com.youyi.rpc.registry.RegistryFactory;
+import com.youyi.rpc.starter.annotation.RpcService;
+import lombok.NonNull;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * RPC 服务提供者启动类
+ * <p>
+ * 获取到包含 @RpcService 注解的类，通过注解的属性和反射机制，获取到要注册的服务信息，完成服务注册
+ *
+ * @author <a href="https://github.com/dingxinliang88">youyi</a>
+ */
+public class RpcProviderBootstrap implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName)
+            throws BeansException {
+        Class<?> beanClazz = bean.getClass();
+        RpcService rpcService = beanClazz.getAnnotation(RpcService.class);
+        // 需要注册服务
+        if (rpcService != null) {
+            // 1. 获取服务基本信息
+            Class<?> interfaceClass = rpcService.interfaceClass();
+            // 默认值处理
+            if (interfaceClass == void.class) {
+                interfaceClass = beanClazz.getInterfaces()[0];
+            }
+
+            String serviceName = interfaceClass.getName();
+            String serviceVersion = rpcService.version();
+            String serviceGroup = rpcService.group();
+
+            // 全局配置
+            final ApplicationConfig applicationConfig = RpcApplication.resolve();
+            // 获取注册中心
+            RegistryConfig registryConfig = applicationConfig.getRegistry();
+            Registry registry = RegistryFactory.getRegistry(registryConfig.getType());
+
+            // 2. 注册服务
+            LocalRegistry.registry(serviceName, beanClazz);
+
+            ServiceMetadata serviceMetadata = new ServiceMetadata();
+            serviceMetadata.setServiceName(serviceName);
+            serviceMetadata.setServiceHost(applicationConfig.getHost());
+            serviceMetadata.setServicePort(applicationConfig.getPort());
+            serviceMetadata.setServiceVersion(serviceVersion);
+            serviceMetadata.setServiceGroup(serviceGroup);
+
+            try {
+                // 注册到注册中心
+                registry.register(serviceMetadata);
+            } catch (Exception e) {
+                throw new RpcException("register service failed", e);
+            }
+        }
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
